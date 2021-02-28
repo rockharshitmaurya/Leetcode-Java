bbfccd583125154819cd09ab0ea4cf3927b16904
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2021-02-28 11:47:54 +0000

    Sync LeetCode submission - Design an Ordered Stream (java)

---
+++ problems/design_an_ordered_stream/solution.java
@@ -0,0 +1,23 @@
+class OrderedStream {
+    int ptr;
+    String values[];
+    public OrderedStream(int n) {
+        values=new String[n];
+        ptr=0;
+    }
+    
+    public List<String> insert(int id, String value) {
+        values[id-1]=value;
+         List<String> result = new ArrayList();
+        while(ptr<values.length && values[ptr]!=null){
+            result.add(values[ptr++]);
+        }
+        return result;
+    }
+}
+
+/**
+ * Your OrderedStream object will be instantiated and called as such:
+ * OrderedStream obj = new OrderedStream(n);
+ * List<String> param_1 = obj.insert(idKey,value);
+ */
