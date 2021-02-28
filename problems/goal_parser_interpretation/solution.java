e9e73e3ea7e55a1cdff757a9a269e634f1e85cdd
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2021-02-28 06:43:43 +0000

    Sync LeetCode submission - Goal Parser Interpretation (java)

---
+++ problems/goal_parser_interpretation/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String interpret(String command) {
+     StringBuilder sb=new StringBuilder();
+  
+    
+  for(int i=0;i<command.length();)
+  {
+      
+      
+      if(command.charAt(i)=='G')
+      {
+          sb.append('G');i++;}
+      
+      else if(command.charAt(i)=='('&&command.charAt(i+1)==')')
+      {
+          sb.append('o');i+=2;
+      }
+    else if(command.charAt(i)=='('&&command.charAt(i+1)=='a')
+      {
+          sb.append("al");i+=4;
+      }
+  }
+    return sb.toString();
+    }
+}
