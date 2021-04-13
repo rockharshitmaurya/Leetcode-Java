b5cc57bd3dc9dab57aa2a6781681f5ba1f6df64d
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2021-04-13 14:05:09 +0000

    Sync LeetCode submission - Minimum Value to Get Positive Step by Step Sum (java)

---
+++ problems/minimum_value_to_get_positive_step_by_step_sum/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minStartValue(int[] nums) {
+        int sum = 0, min_sum = 0;
+    for (var n : nums) {
+        sum += n;
+        min_sum = Math.min(min_sum, sum);
+    }
+    return 1 - min_sum;
+    }
+}
+// 1-3-2-6-8
