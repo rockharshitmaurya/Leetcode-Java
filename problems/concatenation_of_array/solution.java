553e0ea185daac45c120d1ac99e18f2a58153cff
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2021-09-27 19:52:22 +0000

    Sync LeetCode submission - Concatenation of Array (java)

---
+++ problems/concatenation_of_array/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int len=(nums.length*2);
+        int ans[]=new int[len];
+        for(int i=0; i<nums.length; i++){
+            ans[i]=nums[i];
+            ans[i+nums.length]=nums[i];
+        }
+        return ans;
+        
+    }
+}
