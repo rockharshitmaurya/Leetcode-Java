31e338f9c29024483084f14034f597de49cb78f8
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-01-19 09:22:31 +0000

    Sync LeetCode submission - Increasing Order Search Tree (java)

---
+++ problems/increasing_order_search_tree/solution.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode cur=null;
+    public TreeNode increasingBST(TreeNode root) {
+        if(root==null) return root;
+        TreeNode res=new TreeNode(0);
+        cur=res;
+//         TreeNode main=root
+//         increasingBST(root.left);
+//         main.right=root;
+//         increasingBST(root.right);
+        help(root);
+        return res.right;
+    }
+    void help(TreeNode root){
+        if(root==null) return;
+      
+        help(root.left);
+        cur.right=root;
+        root.left=null;
+        cur=root;
+        help(root.right);
+        // root.right=root;
+        // root.left=null;
+    }
+}
