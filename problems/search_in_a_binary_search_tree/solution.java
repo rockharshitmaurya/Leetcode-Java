0cca52d2433e661ea0f55a2d000cfc4a0e40987e
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-02-03 20:05:16 +0000

    Sync LeetCode submission - Search in a Binary Search Tree (java)

--- problems/search_in_a_binary_search_tree/solution.java
+++ problems/search_in_a_binary_search_tree/solution.java
@@ -15,7 +15,8 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        if(root == null || root.val == val) return root;
-        return val<root.val?searchBST(root.left,val):searchBST(root.right,val);
+                if(root==null)  return null;
+                else if(root.val==val) return root;
+                else return (root.val>val?(searchBST(root.left,val)):searchBST(root.right,val));
     }
 }
