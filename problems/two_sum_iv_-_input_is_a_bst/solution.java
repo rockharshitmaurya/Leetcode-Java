171f083697fd80363119d0b6e1b5ba3756b4ec94
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-02-05 14:37:30 +0000

    Sync LeetCode submission - Two Sum IV - Input is a BST (java)

--- problems/two_sum_iv_-_input_is_a_bst/solution.java
+++ problems/two_sum_iv_-_input_is_a_bst/solution.java
@@ -13,13 +13,46 @@
  *     }
  * }
  */
+public class  BSTIterator{
+     Stack<TreeNode> st=new Stack<>();
+        boolean reverse=true;
+        public BSTIterator(TreeNode root,boolean isReverse) {
+            reverse=isReverse;
+            pushAll(root);
+        }
+        private  void pushAll(TreeNode root){
+             while(root!=null){
+                 st.push(root);
+                 if(reverse==true){
+                 root=root.right;   
+                 }else{
+                     root=root.left;
+                 }
+             }
+        }
+        public int next(){
+            TreeNode node=st.pop();
+            if(reverse==true) pushAll(node.left);
+            else pushAll(node.right);
+            return node.val;
+        }
+        private boolean hasNext(TreeNode root){
+            return !st.isEmpty();
+        }
+    }
 class Solution {
-     Set<Integer> set=new HashSet<Integer>();
-    public boolean findTarget(TreeNode root, int k) {
-      if(root==null) return false;
+   
     
-        if(set.contains(k-root.val)) return true;
-        set.add(root.val);
-        return findTarget(root.left,k) || findTarget(root.right,k);
+    public boolean findTarget(TreeNode root, int k) {
+        BSTIterator l=new BSTIterator(root,false);
+        BSTIterator r=new BSTIterator(root,true);
+       int i=l.next();
+        int j=r.next();
+        while(i<j){
+            if((i+j)==k) return true;
+            else if((i+j)<k) i=l.next();
+            else j=r.next();
+        }
+        return false;
     }
 }
