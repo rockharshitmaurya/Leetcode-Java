bbaafe67f804971bcec258e90b22f06f7ce3b34c
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-02-26 20:20:06 +0000

    Sync LeetCode submission - Maximum Binary Tree (java)

--- problems/maximum_binary_tree/solution.java
+++ problems/maximum_binary_tree/solution.java
@@ -15,33 +15,26 @@
  */
 class Solution {
     public TreeNode constructMaximumBinaryTree(int[] nums) {
-        // return helper(nums,0,nums.length-1);
-        Deque<TreeNode> stack=new LinkedList<>();
-        
-        for(int i=0; i<nums.length; i++){
-            TreeNode cur=new TreeNode(nums[i]);
-            while(!stack.isEmpty() && stack.peek().val<nums[i]){
-                cur.left=stack.pop();
-            }
-            if(!stack.isEmpty()){
-                stack.peek().right=cur;
-            }
-            stack.push(cur);
-        }
-        return stack.isEmpty()?null:stack.removeLast();
+               return helper(nums,0,nums.length-1);
     }
-//     TreeNode helper(int arr[], int start,int end){
-//         if(start>end) return null;
-//        int idxMax = end;
-//         for (int i = start; i < end; i++) {
-//             if (arr[i] > arr[idxMax]) {
-//                 idxMax = i;
-//             }
-//         }
-//         TreeNode root=new TreeNode(arr[idxMax]);
-        
-//         root.right = helper(arr, idxMax + 1, end);
-//      root.left = helper(arr, start, idxMax - 1);
-//         return root;
-//     }
+    TreeNode helper(int arr[],int l,int r){
+        if(l>r) return null;
+        int res[]=getMax(arr,l,r);
+        int max=res[0];
+        int idx=res[1];
+        TreeNode root=new TreeNode(max);
+         root.left=helper(arr,l,idx-1);
+        root.right=helper(arr,idx+1,r);
+        return root;
+    }
+    int[] getMax(int nums[],int l,int r){
+        int max=0,idx=l;
+        for(int i=l; i<=r; i++){
+            if(nums[i]>max){
+                max=nums[i];
+                idx=i;
+            }
+        } 
+        return new int[]{max,idx};
+    }
 }
