b36e2634b52068df342cb54629eecdf9edfc5568
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-03-28 08:01:15 +0000

    Sync LeetCode submission - The K Weakest Rows in a Matrix (java)

--- problems/the_k_weakest_rows_in_a_matrix/solution.java
+++ problems/the_k_weakest_rows_in_a_matrix/solution.java
@@ -10,16 +10,6 @@ class Solution {
             ans[idx++][1]=count;
         }
         int ret[]=new int[k];
-//         int min=ans[0][1];
-//         int lo=0;
-//         while(lo<k){
-//             for(int i=ans.length-1; i>=0; i--){
-//                 if(i>vis)
-//                 ans[i][1]
-//             }
-                
-//            lo++;     
-//         }
         Arrays.sort(ans, (a, b) -> a[1] - b[1]);
         for(int i=0; i<k; i++)
             ret[i]=ans[i][0];
