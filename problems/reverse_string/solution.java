1d035bac48f5e08ffe4b63db843a4e49ea24b71e
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-04-01 05:29:55 +0000

    Sync LeetCode submission - Reverse String (java)

--- problems/reverse_string/solution.java
+++ problems/reverse_string/solution.java
@@ -1,15 +1,10 @@
 class Solution {
-    static char[] arr;
     public void reverseString(char[] s) {
-        arr=s;
-        helper(0,s.length-1);
-    }
-    public static void helper(int idx,int last){
-        if(idx>=last) return;
-        char c=arr[idx];
-        arr[idx]=arr[last];
-        arr[last]=c;
-        helper(++idx,--last);
-        
+       int len=s.length;
+        for(int i=0; i*2<len; i++){
+           char temp=s[i];
+           s[i]=s[len-i-1];
+            s[len-i-1]=temp;
+       } 
     }
 }
