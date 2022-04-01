859d4fadb2b7a906a14cfa99548560680a5733ff
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-04-01 10:30:25 +0000

    Sync LeetCode submission - Find Elements in a Contaminated Binary Tree (java)

--- problems/find_elements_in_a_contaminated_binary_tree/solution.java
+++ problems/find_elements_in_a_contaminated_binary_tree/solution.java
@@ -14,27 +14,31 @@
  * }
  */
 class FindElements {
-       private BitSet sets = new BitSet();
-    
-    public FindElements(TreeNode root) {     
-        init(root, 0);
-    }
-    
-    private void init(TreeNode node, int target) {
-      if (node != null)  {
-          
-          node.val = target;
-          sets.set(target);
-          init(node.left, 2*target +1);
-          init(node.right, 2*target +2);
-      }      
+    TreeNode root;
+    public FindElements(TreeNode root) {
+        this.root=root; 
+        // dfs(root,0);
     }
     
     public boolean find(int target) {
-        
-        return sets.get(target);        
+        // System.out.println(root.left.left.val);
+        return dfs(root,0,target);
     }
-}
+    public boolean dfs(TreeNode root,int val,int target){
+        if(root==null) return false;
+            if(val==target) return true;
+            return dfs(root.left,(val*2)+1,target) ||
+            dfs(root.right,(val*2)+2,target);
+        }
+    boolean find(TreeNode root,int val){
+        if(root==null) return false;
+        if(root.val==val) return true;
+        boolean flag1=find(root.left,val);
+        boolean flag2=find(root.right,val);
+        return (flag1 || flag2);
+    }
+    }
+
 
 /**
  * Your FindElements object will be instantiated and called as such:
