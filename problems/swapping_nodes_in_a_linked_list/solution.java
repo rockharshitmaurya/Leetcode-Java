8f7bf2afe3002a8744c08e174504059b45b495f8
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-04-04 08:45:01 +0000

    Sync LeetCode submission - Swapping Nodes in a Linked List (java)

--- problems/swapping_nodes_in_a_linked_list/solution.java
+++ problems/swapping_nodes_in_a_linked_list/solution.java
@@ -10,35 +10,19 @@
  */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-      //  ListNode temp=head,start=null;
-      //   //ListNode swap=new ListNode();
-      //   int len=0;
-      //   while(temp!=null){
-      //       len++;
-      //       if(len==k) start=temp;
-      //       temp=temp.next;
-      //   }
-      //   int loop=0;
-      //   temp=head;
-      //  while(loop!=len-k){
-      //      temp=temp.next;
-      //      loop++;
-      //  }
-      //   int val=0;
-      //   val=temp.val;
-      //   temp.val=start.val;
-      //   start.val=val;
-      // return head;
-        ListNode slow=head,fast=head,start=null;
-        for(int i=1; i<k; i++) fast=fast.next;
-        start=fast;
-        while(fast.next!=null){
+        ListNode slow=head,fast=head,fast2=head;
+        for(int i=1; i<k; i++){
             slow=slow.next;
-            fast=fast.next;
+            fast2=fast2.next;
         }
-        int val=start.val;
-        start.val=slow.val;
-        slow.val=val;
+        while(fast2.next!=null){
+            fast=fast.next;
+            fast2=fast2.next;
+        }
+        int temp=slow.val;
+        slow.val=fast.val;
+        fast.val=temp;
+        
         return head;
     }
 }
