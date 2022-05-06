10ebaf1d843eb035cfd110b3ca6cc16b0cfc24b4
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-05-06 17:25:24 +0000

    Sync LeetCode submission - Remove All Adjacent Duplicates in String II (java)

--- problems/remove_all_adjacent_duplicates_in_string_ii/solution.java
+++ problems/remove_all_adjacent_duplicates_in_string_ii/solution.java
@@ -1,33 +1,38 @@
 class Solution {
     public String removeDuplicates(String s, int k) {
-        Stack<Node> stack=new Stack<>();
-        // StringBuilder str=new StringBuilder();
-        for(char c:s.toCharArray()){
-            if(stack.size()>0 && stack.peek().c==c){
-                // stack.push(c,stack.peek().getValue()+1);
-                stack.peek().count++;
-            } 
-            else{
-               stack.push(new Node(c,1)); 
+        Stack<CharCounter> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!stack.isEmpty() && c == stack.peek().c) {
+                if (stack.peek().count == k - 1) {
+                    stack.pop();
+                } else {
+                    stack.peek().count++;
+                }
+            } else {
+                stack.push(new CharCounter(c));
             }
-            if(stack.peek().count==k) stack.pop();
         }
-        StringBuilder str=new StringBuilder();
-        for(Node n:stack){
-            str.append(String.valueOf(n.c).repeat(n.count));
+        StringBuilder res = new StringBuilder();
+        for (CharCounter charCount : stack) {
+            res.append(charCount);
         }
-      //  System.out.println(stack);
-        return str+"";
-    }
-}
-class Node{
-    char c;
-    int count;
-    public Node(char c, int count){
-        this.c=c;
-        this.count=count;
+        return res.toString();
     }
 }
 
-// aa
-// ddbbdbaa
+
+class CharCounter {
+    char c;
+    int count;
+    
+    CharCounter(char c) {
+        this.c = c;
+        this.count = 1;
+    }
+    
+    @Override
+    public String toString() {
+        return Character.toString(c).repeat(count);
+    }
+}
