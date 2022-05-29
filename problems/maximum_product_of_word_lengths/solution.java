b71a133852fbcbea7f401f0d47690d986c822ad4
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-05-29 08:14:24 +0000

    Sync LeetCode submission - Maximum Product of Word Lengths (java)

--- problems/maximum_product_of_word_lengths/solution.java
+++ problems/maximum_product_of_word_lengths/solution.java
@@ -1,23 +1,26 @@
 class Solution {
     public int maxProduct(String[] words) {
-       if (words == null || words.length == 0)
-		return 0;
-	int len = words.length;
-	int[] value = new int[len];
-	for (int i = 0; i < len; i++) {
-		String tmp = words[i];
-		value[i] = 0;
-		for (int j = 0; j < tmp.length(); j++) {
-			value[i] |= 1 << (tmp.charAt(j) - 'a');
-		}
-	}
-	int maxProduct = 0;
-	for (int i = 0; i < len; i++)
-		for (int j = i + 1; j < len; j++) {
-			if ((value[i] & value[j]) == 0 && (words[i].length() * words[j].length() > maxProduct))
-				maxProduct = words[i].length() * words[j].length();
-		}
-	return maxProduct;
-            
+       // Arrays.sort(words);
+        int ans=0,n=words.length;
+        for(int i=n-1; i>=0; i--){
+            HashSet<Character> set=addSet(words[i]);
+            label:
+            for(int j=i-1; j>=0; j--){
+                for(Character ch:words[j].toCharArray()){
+                    if(set.contains(ch)){
+                       continue label; 
+                    }
+                }
+                ans=Math.max(ans,(words[i].length()*words[j].length()));
+            }
         }
+       return ans; 
+    }
+    HashSet<Character> addSet(String str){
+        HashSet<Character> set=new HashSet<Character>();
+        for(Character ch:str.toCharArray()){
+            set.add(ch);
+        }
+        return set;
+    }
 }
