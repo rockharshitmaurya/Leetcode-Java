85f1dcda1af6d9f85b73678fbe5e15b594ed532e
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-06-04 06:43:56 +0000

    Sync LeetCode submission - N-Queens (java)

--- problems/n-queens/solution.java
+++ problems/n-queens/solution.java
@@ -1,32 +1,31 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-       char[][] board = new char[n][n];
-        for(int i = 0; i < n; i++)
-            for(int j = 0; j < n; j++)
-                board[i][j] = '.';
-        
-        List<List<String>> res = new ArrayList<List<String>>();
-        dfs(board, 0, res);
-        return res;  
-    }
-    
-    void dfs(char chess[][],int row,List<List<String>> res){
-        if(row==chess.length){
-          res.add(construct(chess));
-            return;
-        }
-        for(int col=0; col<chess.length; col++){
-            if(chess[row][col]=='.' && isQueenSafe(chess,row,col)==true){
-                chess[row][col]='Q';
-                dfs(chess,row+1,res);
-                chess[row][col]='.';
+        List<List<String>> ans=new ArrayList<List<String>>();
+        char board[][]=new char[n][n];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                board[i][j]='.';
             }
         }
+        helper(board,ans,0);
+        return ans;
     }
-    
+    void helper(char board[][],List<List<String>> list,int row){
+        if(row==board.length){
+            list.add(answerMake(board));
+            return;
+        }
+        for(int col=0; col<board.length; col++){
+            if(board[row][col]=='.' && isQueenSafe(board,row,col)==true){
+                board[row][col]='Q';
+                helper(board,list,row+1);
+                board[row][col]='.';
+            }
+        }
+        
+    }
     boolean isQueenSafe(char chess[][],int row,int col){
-       
-for(int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--){
+        for(int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--){
             if(chess[i][j] == 'Q'){
                 return false;
             }
@@ -52,18 +51,11 @@ for(int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--){
 
         return true;
     }
-    
-    List<String> construct(char chess[][]){
-        List<String> res=new ArrayList<>();
-        for(int i=0; i<chess.length; i++){
-            res.add(new String(chess[i]));
+    List<String> answerMake(char ch[][]){
+        List<String> ans=new ArrayList<>();
+        for(int i=0; i<ch.length; i++){
+            ans.add(new String(ch[i]));
         }
-        return res;
+        return ans;
     }
-
 }
-// ..Q.
-// .Q..
-// ...Q
-// Q...
-    
