27b5ec7b383cff7c8482513b223a5f552583c299
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-06-12 07:11:17 +0000

    Sync LeetCode submission - Maximum Erasure Value (java)

--- problems/maximum_erasure_value/solution.java
+++ problems/maximum_erasure_value/solution.java
@@ -1,18 +1,19 @@
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
-        HashSet<Integer> set=new HashSet<>();
-        int max=0,res=0,i=0,j=0;
-        while(i<nums.length && j<nums.length){
-            if(!set.contains(nums[j])){
-                res+=nums[j];
-                max=Math.max(max,res);
-                set.add(nums[j++]);
-            }else{
-                res-=nums[i];
-                set.remove(nums[i++]);
+        int prefix[]=new int[nums.length];
+        Map<Integer,Integer> map=new HashMap<>();
+        int pre=0,idx=-1,max=0;
+        for(int i=0; i<nums.length; i++){
+            prefix[i]=pre+nums[i];
+            pre+=nums[i];
+            if(map.containsKey(nums[i])){
+                int num=map.get(nums[i]); 
+                idx=num<idx?idx:num;
             }
+            if(idx==-1) max=prefix[i];
+            else max=Math.max(max,prefix[i]-prefix[idx]);
+            map.put(nums[i],i);
         }
         return max;
     }
 }
-//2-4-4-5-6
