175f4c934aac1f68f777f2f4bf38dce0714673c0
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-06-15 06:23:57 +0000

    Sync LeetCode submission - Longest String Chain (java)

--- problems/longest_string_chain/solution.java
+++ problems/longest_string_chain/solution.java
@@ -1,17 +1,17 @@
 class Solution {
     public int longestStrChain(String[] words) {
-       Map<String, Integer> dp = new HashMap<>();
+        HashMap<String,Integer> dp=new HashMap<>();
         Arrays.sort(words, (a, b)->a.length() - b.length());
-        int res = 0;
-        for (String word : words) {
-            int best = 0;
-            for (int i = 0; i < word.length(); ++i) {
-                String prev = word.substring(0, i) + word.substring(i + 1);
-                best = Math.max(best, dp.getOrDefault(prev, 0) + 1);
+        int res=0;
+        for(String str:words){
+            int best=0;
+            for(int i=0; i<str.length(); i++){
+                String prev=str.substring(0,i)+str.substring(i+1);
+                best=Math.max(best,dp.getOrDefault(prev,0)+1);
             }
-            dp.put(word, best);
-            res = Math.max(res, best);
+            dp.put(str,best);
+            res=Math.max(best,res);
         }
-        return res; 
+        return res;
     }
 }
