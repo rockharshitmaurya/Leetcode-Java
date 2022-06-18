fbcdfbc7631a4f5efba9a543dadf287da7c5e7bc
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-06-18 06:15:29 +0000

    Sync LeetCode submission - Prefix and Suffix Search (java)

--- problems/prefix_and_suffix_search/solution.java
+++ problems/prefix_and_suffix_search/solution.java
@@ -1,41 +1,39 @@
-class TrieNode {
-    TrieNode[] children;
+class TrieNode{
+    TrieNode children[];
     int weight;
-    public TrieNode() {
-        children = new TrieNode[27]; // 'a' - 'z' and '{'. 'z' and '{' are neighbours in ASCII table
-        weight = 0;
+    TrieNode(){
+        children=new TrieNode[27];
+        weight=0;
     }
 }
 class WordFilter {
-
-   TrieNode root;
+    TrieNode root;
     public WordFilter(String[] words) {
-        root = new TrieNode();
-        for (int weight = 0; weight < words.length; weight++) {
-            String word = words[weight] + "{";
-            for (int i = 0; i < word.length(); i++) {
-                TrieNode cur = root;
-                cur.weight = weight;
-    // add "apple{apple", "pple{apple", "ple{apple", "le{apple", "e{apple", "{apple" into the Trie Tree
-                for (int j = i; j < 2 * word.length() - 1; j++) {
-                    int k = word.charAt(j % word.length()) - 'a';
-                    if (cur.children[k] == null)
-                        cur.children[k] = new TrieNode();
-                    cur = cur.children[k];
-                    cur.weight = weight;
+        root=new TrieNode();
+        for(int i=0; i<words.length; i++){
+            String word=words[i]+"{";
+            for(int j=0; j<words[i].length(); j++){
+                TrieNode ptr=root;
+                ptr.weight=i;
+                for(int k=j; k<2*word.length()-1; k++){
+                    // System.out.print(word.charAt(k%word.length()));
+                    int val=word.charAt(k%word.length())-'a';
+                    if(ptr.children[val]==null)
+                        ptr.children[val]=new TrieNode();
+                    ptr=ptr.children[val];
+                    ptr.weight=i;
                 }
+                // System.out.print(" ");
             }
         }
     }
     public int f(String prefix, String suffix) {
-        TrieNode cur = root;
-        for (char c: (suffix + '{' + prefix).toCharArray()) {
-            if (cur.children[c - 'a'] == null) {
-                return -1;
-            }
-            cur = cur.children[c - 'a'];
+        TrieNode ptr=root;
+        for(char c:(suffix+"{"+prefix).toCharArray()){ //ulta pahle suffix fir prefix
+            if(ptr.children[c-'a']==null) return -1;
+            ptr=ptr.children[c-'a'];
         }
-        return cur.weight;
+        return ptr.weight;
     }
 }
 
