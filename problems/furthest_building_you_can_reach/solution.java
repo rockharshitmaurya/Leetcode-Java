e29e59afca3681384dfaa774c3ebb9f5a34f42a7
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-06-21 11:21:22 +0000

    Sync LeetCode submission - Furthest Building You Can Reach (java)

--- problems/furthest_building_you_can_reach/solution.java
+++ problems/furthest_building_you_can_reach/solution.java
@@ -1,15 +1,18 @@
 class Solution {
-    public int furthestBuilding(int[] A, int bricks, int ladders) {
-      PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int i = 0; i < A.length - 1; i++) {
-            int d = A[i + 1] - A[i];
-            if (d > 0)
-                pq.add(d);
-            if (pq.size() > ladders)
-                bricks -= pq.poll();
-            if (bricks < 0)
-                return i;
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        PriorityQueue<Integer> pq=new PriorityQueue<Integer>();
+        for(int i=0; i<heights.length-1; i++){
+            int d=heights[i+1]-heights[i];
+            if(d>0){
+               pq.add(d); 
+            }
+            if(pq.size()>ladders){
+                
+                bricks-=pq.poll();
+            }
+            // System.out.println(bricks+" "+pq.size()); 
+            if(bricks<0) return i;
         }
-        return A.length - 1;  
+        return heights.length-1;
     }
 }
