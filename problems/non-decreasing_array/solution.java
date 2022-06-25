204a7a8967e1bcd90506f7dd2c9c131e324d60c1
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-06-25 06:28:35 +0000

    Sync LeetCode submission - Non-decreasing Array (java)

--- problems/non-decreasing_array/solution.java
+++ problems/non-decreasing_array/solution.java
@@ -1,13 +1,20 @@
 class Solution {
-    public boolean checkPossibility(int[] a) {
-      int modified = 0;
-        for (int i = 1; i < a.length; i++) {
-            if (a[i] < a[i - 1]) {
-                if (modified++ > 0) return false;
-                if (i - 2 < 0 || a[i - 2] <= a[i]) a[i - 1] = a[i]; // lower a[i - 1]
-                else a[i] = a[i - 1]; // rise a[i]
+    public boolean checkPossibility(int[] nums) {
+        for(int i=0; i<nums.length-1; i++){
+            // System.out.println(nums[i]+" "+nums[i+1]);
+            if(nums[i]>nums[i+1]){
+                if(i+2==nums.length) nums[i+1]=nums[i];
+                else if(nums[i+2]>=nums[i]){
+                    nums[i+1]=nums[i];
+                }else{
+                    nums[i]=nums[i+1];
+                }
+                            break;
             }
         }
-        return true;  
+        for(int i=0; i<nums.length-1; i++){
+            if(nums[i]>nums[i+1]) return false;
+        }
+     return true;   
     }
 }
