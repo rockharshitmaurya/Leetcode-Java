42f0c6dc09d82554b92ff26af2f5aa61b489c2cb
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-02 09:35:29 +0000

    Sync LeetCode submission - Unique Paths II (java)

--- problems/unique_paths_ii/solution.java
+++ problems/unique_paths_ii/solution.java
@@ -1,21 +1,19 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] og) {
         int m=og.length,n=og[0].length;
-        int memo[][]=new int[m][n];
-        for(int arr[]:memo)
-            Arrays.fill(arr,-1);
-        return helper(og,m-1,n-1,memo);
-    }
-     int helper(int grid[][],int row,int col,int memo[][]){
-        if(row==0 && col==0){
-            if(grid[0][0]==1) return 0;
-            else return 1;
+        int dp[][]=new int[m][n]; 
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(og[i][j]==1) dp[i][j]=0;
+                else if(i==0 && j==0) dp[i][j]=1;
+                else{
+                    int up=0,left=0;
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+            }
         }
-        if(row<0 || col<0 || grid[row][col]==1) return 0;
-        if(memo[row][col]!=-1) return memo[row][col];
-        int l=helper(grid,row-1,col,memo);
-        int r=helper(grid,row,col-1,memo);
-        memo[row][col]=l+r;
-        return l+r;
+        return dp[m-1][n-1];
     }
 }
