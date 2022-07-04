851bbdbcf4ce7a9f722d20a22207b30dadf4d45a
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-04 03:00:51 +0000

    Sync LeetCode submission - Single Number (java)

--- problems/single_number/solution.java
+++ problems/single_number/solution.java
@@ -1,8 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int xor=0;
-        for(int i=0; i<nums.length; i++)
-        xor=xor^nums[i];
-        return xor;
+        int res=0;
+        for(int num:nums) res=res^num;
+        return res;
     }
 }
