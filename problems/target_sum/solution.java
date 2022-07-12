cea6ee468df2465159a4d1709e56b45945a315ed
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-12 11:17:03 +0000

    Sync LeetCode submission - Target Sum (java)

--- problems/target_sum/solution.java
+++ problems/target_sum/solution.java
@@ -1,15 +1,19 @@
 class Solution {
+    HashMap<String,Integer> map=new HashMap<>();
     public int findTargetSumWays(int[] nums, int target) {
-        return helper(nums,0,target);
+        int n=nums.length;
+        return helper(nums,n-1,target);
     }
     int helper(int nums[],int idx,int target){
-        if(idx==nums.length){
+        if(idx<0){
             if(target==0) return 1;
             else return 0;
         }
-        
-        int left=helper(nums,idx+1,target-nums[idx]);
-        int mid=helper(nums,idx+1,target-(-nums[idx]));
+        String key=idx+":"+target;
+        if(map.containsKey(key)) return map.get(key);
+        int left=helper(nums,idx-1,target-nums[idx]);
+        int mid=helper(nums,idx-1,target-(-nums[idx]));
+        map.put(key,(left+mid));
         return left+mid;
     }
 }
