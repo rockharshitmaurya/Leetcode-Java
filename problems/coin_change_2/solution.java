6910f16f19a398c43f864323073c547672e2c008
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-12 13:08:19 +0000

    Sync LeetCode submission - Coin Change 2 (java)

--- problems/coin_change_2/solution.java
+++ problems/coin_change_2/solution.java
@@ -1,12 +1,32 @@
 class Solution {
+    int dp[][];
     public int change(int amount, int[] coins) {
-      int dp[]=new int[amount+1];
-        dp[0]=1;
-        for(int coin:coins){
-            for(int j=coin; j<dp.length; j++){
-                dp[j]+=dp[j-coin];
+        dp = new int[coins.length][amount+1];
+        // for(int arr[]:dp) Arrays.fill(arr,-1);
+        for(int am=0; am<=amount; am++){
+            if(am%coins[0]==0) 
+                dp[0][am] = 1; 
+        }
+        for(int idx=1; idx<coins.length; idx++){
+            for(int am=0; am<=amount; am++){
+                int take=0;
+                if(coins[idx]<=am) take=dp[idx][am-coins[idx]];
+                int notTake=dp[idx-1][am];
+                dp[idx][am]=(take+notTake);
             }
         }
-        return dp[amount];
+        // return helper(coins,amount,coins.length-1,"");
+        return dp[coins.length-1][amount];
+    }
+    int helper(int coins[],int amount,int idx,String ans){
+        if(idx==0){
+            if(amount%coins[idx]==0) return 1; 
+            return 0;
+        }
+        if(dp[idx][amount]!=-1) return dp[idx][amount];
+        int take=0;
+        if(coins[idx]<=amount) take=helper(coins,amount-coins[idx],idx,ans+" "+coins[idx]);
+        int notTake=helper(coins,amount,idx-1,ans);
+        return dp[idx][amount]=(take+notTake);
     }
 }
