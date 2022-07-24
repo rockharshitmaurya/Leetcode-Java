8b4f88042c2136f6cbc0303658de75816218604a
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-24 07:59:31 +0000

    Sync LeetCode submission - Search a 2D Matrix II (java)

--- problems/search_a_2d_matrix_ii/solution.java
+++ problems/search_a_2d_matrix_ii/solution.java
@@ -1,18 +1,31 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-int row = matrix.length - 1;
-        int col = 0;
+        int n=matrix.length,m=matrix[0].length;
+        int i=0,j=0;
         
-        while (row < matrix.length && row >= 0 && col >= 0 && col < matrix[0].length ) {
-            if (matrix[row][col] == target) {
-                return true;
-            } else if (matrix[row][col] < target) {
-                col++;
-            } else {
-                row--;
+        while(i>=0 && i<n && j>=0 && j<m){
+            if(matrix[i][j]==target) return true;
+            // System.out.println(i+" "+j);
+            if((j+1)<m && matrix[i][j+1]<=target) j++;
+            else if((i+1)<n && matrix[i+1][j]<=target) i++;
+            else {
+                i++;
+                j=0;
             }
+            // System.out.println(i+" "+j);
         }
         
         return false;
+    }
+    int binarySearch(int arr[],int target,int l ,int r){
+       if(l<=r){  
+            int mid=(l+r)>>1;
+           
+            if(arr[mid]==target) return mid;
+
+            if(arr[mid]<target) return binarySearch(arr,target,mid+1,r);
+            return binarySearch(arr,target,l,mid-1);
+       }
+        return -1;
     }
 }
