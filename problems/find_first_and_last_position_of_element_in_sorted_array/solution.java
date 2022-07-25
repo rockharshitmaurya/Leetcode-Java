5538c295f4767489110c76d5807423085ff169d7
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-25 07:51:20 +0000

    Sync LeetCode submission - Find First and Last Position of Element in Sorted Array (java)

--- problems/find_first_and_last_position_of_element_in_sorted_array/solution.java
+++ problems/find_first_and_last_position_of_element_in_sorted_array/solution.java
@@ -1,19 +1,31 @@
 class Solution {
+    int start=-1,end=-1;
     public int[] searchRange(int[] nums, int target) {
-        int res[]={-1,-1};
-        boolean start=false,end=false;
-        int len=nums.length;
-        for(int i=0; i<len; i++){
-            if(nums[i]==target && start==false){
-                res[0]=i;
-                start=true;
-            }
-           if(nums[len-i-1]==target && end==false){
-               res[1]=len-i-1;
-               end=true;
-           }
-            if(start==true && end==true) break;
+        int idx=binarySearch_left(nums,target,0,nums.length-1,-1);
+        int idx2=binarySearch_right(nums,target,0,nums.length-1,-1);
+       return new int[]{idx,idx2}; 
+    }
+    int binarySearch_left(int nums[],int target,int l,int r,int idx){
+        if(l<=r){
+            int mid=(l+r)>>1;
+            if(nums[mid]==target) idx=mid;
+            
+            if(nums[mid]<target) return binarySearch_left(nums,target,mid+1,r,idx);
+            
+            return binarySearch_left(nums,target,l,mid-1,idx);
         }
-        return res;
+        return idx;
+    }
+    
+    int binarySearch_right(int nums[],int target,int l,int r,int idx){
+        if(l<=r){
+            int mid=(l+r)>>1;
+            if(nums[mid]==target) idx=mid;
+            
+            if(nums[mid]<=target) return binarySearch_right(nums,target,mid+1,r,idx);
+            
+            return binarySearch_right(nums,target,l,mid-1,idx);
+        }
+        return idx;
     }
 }
