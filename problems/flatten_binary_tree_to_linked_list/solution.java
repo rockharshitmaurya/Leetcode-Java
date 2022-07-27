bbe23ebaf8baa9979904469f1ecac7fb7abc6867
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-27 06:09:58 +0000

    Sync LeetCode submission - Flatten Binary Tree to Linked List (java)

--- problems/flatten_binary_tree_to_linked_list/solution.java
+++ problems/flatten_binary_tree_to_linked_list/solution.java
@@ -14,21 +14,23 @@
  * }
  */
 class Solution {
+    
     public void flatten(TreeNode root) {
         TreeNode cur=root;
+        
         while(cur!=null){
-            
             if(cur.left!=null){
-               TreeNode prev=cur.left;
-                while(prev.right!=null){
-                    prev=prev.right;
-                }
-                prev.right=cur.right;
+                TreeNode temp=cur.left;
+                
+                while(temp.right!=null) temp=temp.right;
+                
+                temp.right=cur.right;
                 cur.right=cur.left;
                 cur.left=null;
-                
             }
             cur=cur.right;
         }
+        
+        
     }
 }
