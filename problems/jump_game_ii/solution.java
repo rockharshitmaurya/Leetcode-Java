ba6caec2abec0a9dfcfbb706bedf8fe016a3ea60
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-28 07:13:27 +0000

    Sync LeetCode submission - Jump Game II (java)

--- problems/jump_game_ii/solution.java
+++ problems/jump_game_ii/solution.java
@@ -1,15 +1,29 @@
 class Solution {
-    public int jump(int[] A) {
-        int step_count = 0;
-        int last_jump_max = 0;
-        int current_jump_max = 0;
-        for(int i=0; i<A.length-1; i++) {
-            current_jump_max = Math.max(current_jump_max, i+A[i]);
-            if( i == last_jump_max ) {
-                step_count++;
-                last_jump_max = current_jump_max;
-            } 
+    public int jump(int[] nums) {
+        int n=nums.length;
+        int dp[]=new int[n];
+        Arrays.fill(dp,(int)1e8);
+        dp[n-1]=0;
+        
+        for(int idx=n-2; idx>=0; idx--){
+            int jump=(int)1e8; 
+            if(nums[idx]==0) continue;
+            for(int i=idx+1; i<=Math.min((idx+nums[idx]),n-1); i++)
+                jump=Math.min(jump,1+dp[i]);
+            dp[idx]=jump;
         }
-        return step_count;  
+        
+        return dp[0];
+    }
+    
+    int helper(int nums[],int idx){
+        if(idx==nums.length-1) return 0;
+        if(idx>=nums.length) return (int)1e8;
+        if(nums[idx]==0) return (int)1e8;
+        int jump=(int)1e8;
+        
+        for(int i=idx+1; i<=(idx+nums[idx]); i++)
+            jump=Math.min(jump,1+helper(nums,i));
+        return jump;
     }
 }
