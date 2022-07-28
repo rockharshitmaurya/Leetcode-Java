b81cee38071da667f55d13a0445d60aa1683c566
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-28 04:27:00 +0000

    Sync LeetCode submission - Delete and Earn (java)

--- problems/delete_and_earn/solution.java
+++ problems/delete_and_earn/solution.java
@@ -1,14 +1,17 @@
 class Solution {
+
     public int deleteAndEarn(int[] nums) {
-      int[] sum = new int[10002]; 
+        int max=0;
+        for(int num:nums) 
+            max=Math.max(max,num);
+        int dp[]=new int[max+1];
         
-        for(int i = 0; i < nums.length; i++){
-            sum[nums[i]] += nums[i]; 
-        }
+        for(int num:nums)
+            dp[num]+=num;
         
-        for(int i = 2; i < sum.length; i++){
-            sum[i] = Math.max(sum[i-1], sum[i-2] + sum[i]);
+        for(int i=2; i<dp.length; i++){
+            dp[i]=Math.max(dp[i-1],dp[i-2]+dp[i]);
         }
-        return sum[10001];  
+        return dp[max];
     }
 }
